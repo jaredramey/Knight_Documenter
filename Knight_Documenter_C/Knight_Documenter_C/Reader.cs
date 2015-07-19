c2e38aa89d85b82f24fbfdf9a42bf6f7de563f9a
Jared Ramey <jared.ramey@students.aie.edu.au>
2015-07-19 00:38:22 -0700

    Project Start
    The official start to the project after realizing that the first start
    ended up be visual basic instead of c#. So after correcting that I am
    finally starting after so long of not having time to.

---
+++ Knight_Documenter_C/Knight_Documenter_C/Reader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Knight_Documenter_C
+{
+    /*
+     * This class is to be designed for the use of opening, reading and extracting information/ text
+     * from C# and Cpp files
+     */
+
+    class Reader
+    {
+        //Buffer for file reading, edit to improve reading
+        const Int32 BufferSize = 128;
+
+        //public functions
+
+
+    }
+}
