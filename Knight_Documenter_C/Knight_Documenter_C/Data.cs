c1db56cca901d497ccb7bbbdac63a761921ad420
Jared <rameyjared@gmail.com>
2016-11-16 21:18:41 -0800

    Work on Data class
    Starting working on the data class and getting it set up how I want. Now
    time to move onto reader class and see if data is set up right.
    
    TODO for Knight Documenter
    -Figure out approach for data extraction
    -Figure out how I want to store the data
    -Fill out reader class
    -Edit data class
    -Figure out architechture for VisualDoc and TextDoc classes
    -Fill out TextDoc class
    -Fill out Draw class
    -Fill out VisualDoc class

--- Knight_Documenter_C/Knight_Documenter_C/Data.cs
+++ Knight_Documenter_C/Knight_Documenter_C/Data.cs
@@ -8,5 +8,55 @@ namespace Knight_Documenter_C
 {
     class Data
     {
+        List<ClassStorage> data = new List<ClassStorage>();
+
+        //Clears all current data
+        private void ClearData()
+        {
+            data.Clear();
+        }
+
+        //Find a class by name
+        public ClassStorage GetClassByName(string className)
+        {
+            if (data != null && data.Count > 0)
+            {
+                return data.Find(x => x.name == className);
+            }
+
+            else
+            {
+                return null;
+            }
+        }
+
+        //returns all currently stored data
+        public List<ClassStorage> GetAllData()
+        {
+            if(data != null && data.Count > 0)
+            {
+                return data;
+            }
+
+            else
+            { 
+                return null;
+            }
+        }
     }
+
+    
+}
+
+//Reflection to store data and extract how I need to
+private class ClassStorage
+{
+    //Store Class name
+    public string name;
+    //Store function names
+    public List<string> functions;
+    //Store refrences from class to class
+    public List<string> functionRefrences;
+    //Store all comments from class
+    public List<string> comments;
 }
